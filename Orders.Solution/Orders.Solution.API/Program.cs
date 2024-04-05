4db5a33bbe4918ace29585fbd3a6c6c91b29ed73
cmontenegro <cmontenegro>
2024-04-05 15:22:52 -0500

    Api connection

---
+++ Orders.Solution/Orders.Solution.API/Program.cs
@@ -0,0 +1,72 @@
+using Orders.Solution.Core.Data;
+using Microsoft.EntityFrameworkCore;
+using Orders.Solution.Core.Init;
+using Orders.Solution.Core.ObjectRepository.Interface;
+using Orders.Solution.Core.ObjectRepository.Implementation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("OrdersConectionDB");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IDBInitializer, DBInitializer>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IIdentificationTypeRepository, IdentificationTypeRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<IOrderHeaderRepository, OrderHeaderRepository>();
+builder.Services.AddScoped<IOrderDtlRepository, OrderDtlRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors("AllowOrigin");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+    try
+    {
+        var initializer = services.GetRequiredService<IDBInitializer>();
+        initializer.Initialize();
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "Error in migration");
+    }
+}
+
+app.MapControllers();
+
+app.Run();
--- Orders.Solution/Orders.Solution.Core/Init/DBInitializer.cs
+++ Orders.Solution/Orders.Solution.Core/Init/DBInitializer.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Solution.Core.Data;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Orders.Solution.Core.Init
 {
@@ -30,32 +27,32 @@ namespace Orders.Solution.Core.Init
                 throw;
             }
 
-            if (!_db.ProcessPpal.Any())
-            {
-                var LProcessDataJson = File.ReadAllText("../Matrix.Multiplication.AccessData/Data/SeedData/ProcessPpal.json");
-                var LProcess = JsonSerializer.Deserialize<List<ProcessPpal>>(LProcessDataJson);
-                _db.ProcessPpal.AddRange(LProcess);
-            }
+            //if (!_db.ProcessPpal.Any())
+            //{
+            //    var LProcessDataJson = File.ReadAllText("../Matrix.Multiplication.AccessData/Data/SeedData/ProcessPpal.json");
+            //    var LProcess = JsonSerializer.Deserialize<List<ProcessPpal>>(LProcessDataJson);
+            //    _db.ProcessPpal.AddRange(LProcess);
+            //}
 
-            if (_db.ChangeTracker.HasChanges())
-                _db.SaveChanges();
+            //if (_db.ChangeTracker.HasChanges())
+            //    _db.SaveChanges();
 
-            if (!_db.ProcessMatrix.Any())
-            {
-                var LProcessMatrixDataJson = File.ReadAllText("../Matrix.Multiplication.AccessData/Data/SeedData/ProcessMatrix.json");
-                var LProcessMatrix = JsonSerializer.Deserialize<List<ProcessMatrix>>(LProcessMatrixDataJson);
-                _db.ProcessMatrix.AddRange(LProcessMatrix);
-            }
+            //if (!_db.ProcessMatrix.Any())
+            //{
+            //    var LProcessMatrixDataJson = File.ReadAllText("../Matrix.Multiplication.AccessData/Data/SeedData/ProcessMatrix.json");
+            //    var LProcessMatrix = JsonSerializer.Deserialize<List<ProcessMatrix>>(LProcessMatrixDataJson);
+            //    _db.ProcessMatrix.AddRange(LProcessMatrix);
+            //}
 
-            if (_db.ChangeTracker.HasChanges())
-                _db.SaveChanges();
+            //if (_db.ChangeTracker.HasChanges())
+            //    _db.SaveChanges();
 
-            if (!_db.ProcessMatrixDetail.Any())
-            {
-                var LProcessMatrixDetailDataJson = File.ReadAllText("../Matrix.Multiplication.AccessData/Data/SeedData/ProcessMatrixDetail.json");
-                var LProcessMatrixDetail = JsonSerializer.Deserialize<List<ProcessMatrixDetail>>(LProcessMatrixDetailDataJson);
-                _db.ProcessMatrixDetail.AddRange(LProcessMatrixDetail);
-            }
+            //if (!_db.ProcessMatrixDetail.Any())
+            //{
+            //    var LProcessMatrixDetailDataJson = File.ReadAllText("../Matrix.Multiplication.AccessData/Data/SeedData/ProcessMatrixDetail.json");
+            //    var LProcessMatrixDetail = JsonSerializer.Deserialize<List<ProcessMatrixDetail>>(LProcessMatrixDetailDataJson);
+            //    _db.ProcessMatrixDetail.AddRange(LProcessMatrixDetail);
+            //}
 
             if (_db.ChangeTracker.HasChanges())
                 _db.SaveChanges();
