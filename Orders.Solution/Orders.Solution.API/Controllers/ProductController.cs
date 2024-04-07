27796eff3e64ddb7cdc95cedeef221a8d8ecc475
ccmmasi <ccmmasi@hotmail.com>
2024-04-07 17:46:05 -0500

    remove code

--- Orders.Solution/Orders.Solution.API/Controllers/ProductController.cs
+++ Orders.Solution/Orders.Solution.API/Controllers/ProductController.cs
@@ -30,31 +30,6 @@ namespace Orders.Solution.API.Controllers
         {
             var products = await _productRepository.GetProductsByCategoryId(categoryId, page, sizePage, sorting);
             return Ok(products);
-        }
-
-        //// GET api/<ValuesController>/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/<ValuesController>
-        //[HttpPost]
-        //public void Post([FromBody] string value)
-        //{
-        //}
-
-        //// PUT api/<ValuesController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
-        //// DELETE api/<ValuesController>/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
+        } 
     }
 }
