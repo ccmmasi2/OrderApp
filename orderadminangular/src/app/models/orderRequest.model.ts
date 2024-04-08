489380983fcb3205ed6d8446fced78eb48c87d35
ccmmasi <ccmmasi@hotmail.com>
2024-04-08 15:25:27 -0500

    OrderRequest
    create object to send

---
+++ orderadminangular/src/app/models/orderRequest.model.ts
@@ -0,0 +1,7 @@
+import { CustomerDTO } from "./customer.model";
+import { ProductDTO } from "./product.model";
+
+export class OrderRequest {
+    lProducts: ProductDTO[] = []; 
+    customer: CustomerDTO = new CustomerDTO(); 
+}
--- orderadminangular/src/app/modules/shared/generate-order/generate-order.component.ts
+++ orderadminangular/src/app/modules/shared/generate-order/generate-order.component.ts
@@ -1,8 +1,10 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { AppComponent } from '@app/app.component';
+import { CustomerDTO } from '@app/models/customer.model';
 import { IdentificationTypeDTO } from '@app/models/identificationType.model';
+import { OrderRequest } from '@app/models/orderRequest.model';
 import { ProductDTO } from '@app/models/product.model';
+import { AlertService } from '@app/services/alert-service.service';
 import { ApiConnectionService } from '@app/services/api-connection.service';
 
 @Component({
@@ -23,14 +25,15 @@ export class GenerateOrderComponent implements OnInit {
   identification: string = '';
   name: string = '';
   lastName: string = '';
-  birthDay: string = '';
+  birthDay!: Date;
   email: string = '';
   phoneNumber: string = '';
   shippingAddress: string = '';
   
   constructor(
     public apiConnectionService: ApiConnectionService,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private alertService: AlertService
   ) {
     this.totalQty = data.totalQty;
     this.totalSum = data.totalSum;
@@ -48,13 +51,42 @@ export class GenerateOrderComponent implements OnInit {
       }
       else {
         const message = `Error loading identification types`
+        this.alertService.showAlert(message, 'error'); 
       }
     },
     (error) => {
       const message = `Error loading identification types: "${error}"`
+      this.alertService.showAlert(message, 'error'); 
     })
+  } 
+
+  validateAge(birthDay: Date): boolean {
+    const currentDate = new Date();
+    const dateOfBirth = birthDay;
+    const diff = currentDate.getTime() - dateOfBirth.getTime();
+    const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+    return age >= 18;
   }
 
   submitForm(): void {
+    if (!this.validateAge(this.birthDay)) {
+      const message = `The customer must be over than 18 years old`
+      this.alertService.showAlert(message, 'error'); 
+      return;
+    }
+    else {
+      const orderRequest: OrderRequest = {
+        customer: {
+          name: this.name,
+          lastName: this.lastName, 
+          identificationTypeId: this.identificationTypeId, 
+          identification: this.identification,
+          birthDay: this.birthDay,
+          email: this.email, 
+          phoneNumber: this.phoneNumber
+        },
+        lProducts: this.cartItems
+      }
+    }
   }
 }
